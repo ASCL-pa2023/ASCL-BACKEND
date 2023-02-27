f0b5b15080f2c4c43c44bef056445157675250c7
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2023-02-27 19:41:25 +0100

    tournament

--- src/main/java/esgi/ascl/Tournament/Entitie/Tournament.java
+++ src/main/java/esgi/ascl/Tournament/Entitie/Tournament.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.util.Date;
+
 @Getter
 @Entity
 @Table(name = "tournament")
@@ -14,5 +16,15 @@ public class Tournament {
     @Column(name = "id", updatable = false)
     private int id;
 
+    @Column(name = "location")
+    private String location;
 
+    @Column(name = "start_date")
+    private Date start_date;
+
+    @Column(name = "end_date")
+    private Date end_date;
+
+    @Column(name = "deadline_inscription_date")
+    private Date deadline_inscription_date;
 }
