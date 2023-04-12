56091cdf3a731786dcd0071c82f0167e80fb0e3e
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2023-04-12 14:24:41 +0200

    CICD test

--- src/test/java/esgi/ascl/AsclApplicationTests.java
+++ src/test/java/esgi/ascl/AsclApplicationTests.java
@@ -1,11 +1,20 @@
 package esgi.ascl;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
 @SpringBootTest
 class AsclApplicationTests {
-//test
+
+    @Autowired
+    TestEntityManager entityManager;
     @Test
     void contextLoads() {
     }
