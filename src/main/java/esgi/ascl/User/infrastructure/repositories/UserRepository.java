80f8c13abc956d1806f7d2c201f14a75c46cb0f7
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2023-04-15 14:39:31 +0200

    manque des config / verification

--- src/main/java/esgi/ascl/User/infrastructure/repositories/UserRepository.java
+++ src/main/java/esgi/ascl/User/infrastructure/repositories/UserRepository.java
@@ -4,8 +4,10 @@ import esgi.ascl.User.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User getUserById(Long id);
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
