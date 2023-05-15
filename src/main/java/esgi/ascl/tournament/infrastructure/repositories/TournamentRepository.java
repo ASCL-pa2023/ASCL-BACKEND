0887d58b327004df74efa0154f94fc529dcf787b
kelyan-9560 <kelyan.bervin@gmail.com>
2023-05-15 20:04:08 +0200

    Update TournamentRepository.java

--- src/main/java/esgi/ascl/tournament/infrastructure/repositories/TournamentRepository.java
+++ src/main/java/esgi/ascl/tournament/infrastructure/repositories/TournamentRepository.java
@@ -3,10 +3,12 @@ package esgi.ascl.tournament.infrastructure.repositories;
 import esgi.ascl.tournament.domain.entitie.Tournament;
 import esgi.ascl.tournament.domain.entitie.TournamentType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface TournamentRepository extends JpaRepository<Tournament, Long> {
     Tournament getTournamentById(Long id);
 
@@ -14,5 +16,5 @@ public interface TournamentRepository extends JpaRepository<Tournament, Long> {
 
     List<Tournament> getTournamentsByLocation(String location);
 
-    List<Tournament> getTournamentsByDate(Date date);
+    //List<Tournament> getTournamentsByDate(Date date);
 }
