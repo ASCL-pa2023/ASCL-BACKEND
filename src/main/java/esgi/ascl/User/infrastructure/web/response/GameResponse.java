9567ac9ad24c8aa5db950dbd423771fb36eb4315
kelyan-9560 <kelyan.bervin@gmail.com>
2023-06-11 23:55:22 +0200

    add List<Team> into GameResponses

--- src/main/java/esgi/ascl/User/infrastructure/web/response/GameResponse.java
+++ src/main/java/esgi/ascl/User/infrastructure/web/response/GameResponse.java
@@ -1,6 +1,10 @@
 package esgi.ascl.User.infrastructure.web.response;
 
+import esgi.ascl.game.domain.entities.GameType;
+import esgi.ascl.game.infra.web.response.TeamResponse;
 import org.joda.time.DateTime;
+
+import java.util.List;
 
 public class GameResponse {
 
@@ -9,6 +13,8 @@ public class GameResponse {
     public Long refereeId;
     public DateTime hourly;
     public Long winner_id;
+    public GameType type;
+    public List<TeamResponse> teams;
 
 
     public Long getId() {
@@ -53,6 +59,24 @@ public class GameResponse {
 
     public GameResponse setWinner_id(Long winner_id) {
         this.winner_id = winner_id;
+        return this;
+    }
+
+    public GameType getType() {
+        return type;
+    }
+
+    public GameResponse setType(GameType type) {
+        this.type = type;
+        return this;
+    }
+
+    public List<TeamResponse> getTeams() {
+        return teams;
+    }
+
+    public GameResponse setTeams(List<TeamResponse> teams) {
+        this.teams = teams;
         return this;
     }
 
--- src/main/java/esgi/ascl/game/domain/mapper/GameMapper.java
+++ src/main/java/esgi/ascl/game/domain/mapper/GameMapper.java
@@ -1,11 +1,23 @@
 package esgi.ascl.game.domain.mapper;
 
+import esgi.ascl.User.domain.mapper.UserMapper;
 import esgi.ascl.User.infrastructure.web.response.GameResponse;
 import esgi.ascl.game.domain.entities.Game;
+import esgi.ascl.game.domain.service.GameService;
+import esgi.ascl.game.domain.service.PlayService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameMapper {
+
+    private static GameService gameService;
+    private static TeamMapper teamMapper;
+
+
+    public GameMapper(GameService gameService, TeamMapper teamMapper) {
+        GameMapper.gameService = gameService;
+        this.teamMapper = teamMapper;
+    }
 
     public static GameResponse entityToResponse(Game game) {
         return new GameResponse()
@@ -13,7 +25,14 @@ public class GameMapper {
                 .setTournamentId(game.getTournament().getId())
                 .setRefereeId(game.getReferee() == null ? null : game.getReferee().getId())
                 .setHourly(game.getHourly())
-                .setWinner_id(game.getWinner_id());
+                .setWinner_id(game.getWinner_id())
+                .setType(game.getType())
+                .setTeams(
+                        gameService.getTeams(game.getId())
+                                .stream()
+                                .map(team -> teamMapper.toResponse(team))
+                                .toList()
+                );
     }
 
 }
